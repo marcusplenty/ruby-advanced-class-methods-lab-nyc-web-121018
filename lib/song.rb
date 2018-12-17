f244a7322e06914fdea84173eeecbb3bcea9e78f
Marcus Plenty <marcusplenty@gmail.com>
2018-12-17 05:57:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,11 +37,13 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    if self.all.include?(name)
-      self.find_by_name(name)
-    else
+    if self.find_by_name(name) == false
       self.create_by_name(name)
-    end 
-  end 
+    else
+      self.find_by_name(name)
+    end
+  end
+    
+      
 
 end
